e2b723d47a3df7cf0131d8ec428e45213f75369c
jbillote <jbillote@gmail.com>
2025-04-16 21:09:55 -0700

    Add channel banner

--- apps/frontend/src/app/[streamer]/page.tsx
+++ apps/frontend/src/app/[streamer]/page.tsx
@@ -1,9 +1,20 @@
+import { API } from 'libs'
+import Image from 'next/image'
+
 export default async function Page({ params }: { params: Promise<{ streamer: string }> }) {
     const { streamer } = await params
+    // TODO: Error checking
+    const { data, error } = await API.api.channel({ member: streamer }).get()
 
     return (
-        <h1 className="flex flex-row min-h-screen justify-center items-center text-4xl">
-            {decodeURIComponent(streamer)}
-        </h1>
+        <div className="h-[calc(100vw/6.2-1px)]">
+            <Image
+                src={data?.banner! + '=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj'}
+                width={1707}
+                height={282}
+                alt={data?.name!}
+                className="w-full max-h-fit bg-cover"
+            />
+        </div>
     )
 }
