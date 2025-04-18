afa7c5833cbae14b4b5190b08b37a054f2a820b7
jbillote <jbillote@gmail.com>
2025-04-18 16:28:06 -0700

    Make scheduledStart optional to deal with shorts

--- apps/backend/src/models/video.ts
+++ apps/backend/src/models/video.ts
@@ -6,7 +6,7 @@ const video = t.Object({
   type: t.String(),
   publishedAt: t.Date(),
   availableAt: t.Date(),
-  scheduledStart: t.Date(),
+  scheduledStart: t.Optional(t.Date()),
   duration: t.Number(),
   status: t.String(),
   thumbnail: t.String(),
