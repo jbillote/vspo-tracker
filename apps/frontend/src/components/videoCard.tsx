238ededfce063c208e447dbe1a9ac0471a4b8409
jbillote <jbillote@gmail.com>
2025-04-19 14:03:55 -0700

    Add relative timestamps to video cards

--- apps/frontend/src/components/videoCard.tsx
+++ apps/frontend/src/components/videoCard.tsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+'use server'
+
+import { DateTime } from 'luxon'
 import Link from 'next/link'
 
 type videoCardProps = {
@@ -6,31 +8,38 @@ type videoCardProps = {
     title: string,
     type: string,
     membersOnly: boolean,
-    publishedAt: Date,
-    availableAt: Date,
-    scheduledStart?: Date,
+    publishedAt: string,
+    availableAt: string,
+    scheduledStart?: string,
     duration: number,
     status: string,
     thumbnail: string,
 }
 
-export default function VideoCard({ video }: { video: videoCardProps }) {
+export default async function VideoCard({ video }: { video: videoCardProps }) {
+    const scheduledStart = video.scheduledStart ? DateTime.fromISO(video.scheduledStart) : null
+
     return (
-        <div className="p-2 m-1 bg-accent rounded-md">
+        <div className="m-1 bg-accent rounded-md inline-block">
             <Link href={`https://youtube.com/watch?v=${video.id}`}>
-                <Image
-                    src={video.thumbnail}
-                    width={1280}
-                    height={720}
-                    alt={video.title}
-                    className="w-64"
+                <div
+                    style={{
+                        // @ts-expect-error: TailwindCSS property, not built-in to React definitions
+                        '--image-url': `url(${video.thumbnail})`
+                    }}
+                    className="w-64 h-36 rounded-md bg-[image:var(--image-url)] bg-cover bg-center bg-no-repeat"
                 />
             </Link>
-            <Link href={`https://youtube.com/watch?v=${video.id}`}>
-                <div className="inline-block max-w-64 text-sm line-clamp-1">
-                    {video.title}
-                </div>
-            </Link>
+            <div className="my-2 mx-2">
+                <Link href={`https://youtube.com/watch?v=${video.id}`}>
+                    <span className="inline-block max-w-64 font-bold text-sm line-clamp-2 truncate">
+                        {video.title}
+                    </span>
+                    <div className="max-w-64 text-sm">
+                        {scheduledStart?.toRelative()}
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
